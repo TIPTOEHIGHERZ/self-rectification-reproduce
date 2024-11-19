c9a86e1a4ff4589fc1aabb8684e136253b546ddb
tiptoe <zhangzeyutiptoe@gmail.com>
2024-11-19 16:44:59 +0800

    to test register forward function

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from diffusers import DiffusionPipeline
+from diffusers import DiffusionPipeline, DDIMScheduler
 from model.pipeline import SelfRectificationPipeline
 from model.KVInjection import KVSaver
 
 
 model_path = './pretrained/stable-diffusion-v1-4'
 
-pipe: SelfRectificationPipeline = DiffusionPipeline.from_pretrained(model_path)
+scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear",
+                          clip_sample=False,
+                          set_alpha_to_one=False)
+pipe: SelfRectificationPipeline = SelfRectificationPipeline.from_pretrained(model_path, scheduler=scheduler)
 KVSaver.register_kv_saver(pipe)
